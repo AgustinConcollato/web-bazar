57d5f7e58e9609a13d3c7d4c15cd9ace7dc64146
Agustín Concollato <agustinconcollato@gmail.com>
2024-11-21 02:25:42 -0300

    configuración app

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Outlet, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Login } from './components/Auth/Login/Login'
+import { AuthContext } from './context/authContext'
+import { useContext } from 'react'
+import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute'
+import { Header } from './components/Header/Header'
+import { Register } from './components/Auth/Register/Register'
+
+export function App() {
+
+  const { user } = useContext(AuthContext)
+
+  function ProtectedLayout({ isUser }) {
+    return (
+      <ProtectedRoute isUser={isUser}>
+        <Outlet />
+      </ProtectedRoute>
+    )
+  }
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/iniciar-sesion' element={<Login />} />
+        <Route path='/registrarse' element={<Register />} />
+        <Route path='/productos/:categoryCode' element={<></>} />
+        <Route path='/pedido' element={<></>} />
+        <Route path='*' element={<>no se encontró</>} />
+      </Routes>
+    </>
+  )
+}
+
