c5d28eef9a5924d446d874d12b02cdc8cdd629cf
Agustín Concollato <agustinconcollato@gmail.com>
2024-12-06 10:27:02 -0300

    adaptar el diesño de card del producto para cuando se agrega el producto

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,27 +1,22 @@
 import { urlStorage } from "api-services";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
-import './ProductCard.css'
+import { FormAddProductToCart } from "../FormAddProductToCart/FormAddProductToCart";
+import './ProductCard.css';
 
 export function ProductCard({ e }) {
     return (
-        <div className='productCard'>
-            <Link to={'/producto/' + e.id}>
-                <img src={urlStorage + '/' + JSON.parse(e.images)[0]} alt="" />
-                <div>
-                    <p>{e.name}</p>
-                    <p className='description'>{e.description}</p>
-                </div>
-                <span>${e.price}</span>
-            </Link>
-            <form>
-                <input type="number" name="quantity" min={1} className="input" placeholder='Cantidad' />
-                <div>
-                    <button type='submit' className="btn btn-regular"><FontAwesomeIcon icon={faBasketShopping} /></button>
-                    <span>Agregar al pedido</span>
-                </div>
-            </form>
-        </div>
+        <>
+            <div className='product-card'>
+                <Link to={'/producto/' + e.id}>
+                    <img loading="lazy" src={urlStorage + '/' + JSON.parse(e.images)[0]} alt={e.name + e.description} />
+                    <div>
+                        <p>{e.name}</p>
+                        <p className='description'>{e.description}</p>
+                    </div>
+                    <span>${e.price}</span>
+                </Link>
+                <FormAddProductToCart type={'CARD'} product={e} />
+            </div>
+        </>
     )
 }
