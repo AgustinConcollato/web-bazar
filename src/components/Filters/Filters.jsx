ad90a2b769dd42b8b2d594e49841213ad75b6f54
Agustín Concollato <agustinconcollato@gmail.com>
2025-01-06 18:53:50 -0300

    refactor: enhance Filters component for better responsiveness and subcategory visibility

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -9,6 +9,7 @@ export function Filters({ category }) {
     const { subcategoryCode, categoryCode } = useParams()
 
     const [categoryData, setCategoryData] = useState(null)
+    const [hidden, setHidden] = useState(false)
 
     async function getCategories() {
         const categories = new Categories()
@@ -19,9 +20,22 @@ export function Filters({ category }) {
         document.title = `Productos de ${response.category_name}`
     }
 
-    useEffect(() => {
 
+    useEffect(() => {
         getCategories()
+
+        const handleResize = () => {
+            if (window.innerWidth > 850) setHidden(false);
+            else setHidden(true);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, [category])
 
     return (
@@ -30,21 +44,23 @@ export function Filters({ category }) {
                 <>
                     <h1>{categoryData.category_name}</h1>
                     <div>
-                        <p>Subcategorías </p>
+                        <p onClick={() => setHidden(!hidden)}>Subcategorías </p>
                         {subcategoryCode && <Link to={'/productos/' + categoryCode} className="btn btn-error-thins" >Borrar filtro</Link>}
                     </div>
-                    <ul>
-                        {categoryData.subcategories.map((e) =>
-                            <li key={e.subcategory_code}>
-                                <Link
-                                    to={'/productos/' + categoryCode + '/' + e.subcategory_code}
-                                    className={subcategoryCode == e.subcategory_code ? 'subcategorySelected' : ''}
-                                >
-                                    {e.subcategory_name}
-                                </Link>
-                            </li>
-                        )}
-                    </ul>
+                    {!hidden &&
+                        <ul onClick={() => setHidden(!hidden)}>
+                            {categoryData.subcategories.map((e) =>
+                                <li key={e.subcategory_code}>
+                                    <Link
+                                        to={'/productos/' + categoryCode + '/' + e.subcategory_code}
+                                        className={subcategoryCode == e.subcategory_code ? 'subcategorySelected' : ''}
+                                    >
+                                        {e.subcategory_name}
+                                    </Link>
+                                </li>
+                            )}
+                        </ul>
+                    }
                 </>
             }
         </aside >
