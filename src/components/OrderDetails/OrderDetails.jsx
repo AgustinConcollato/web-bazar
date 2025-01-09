325a4d2e3eefc24cb6b23c4e08e7bb446bbbf21b
Agustín Concollato <agustinconcollato@gmail.com>
2025-01-09 15:59:50 -0300

    feat: add OrderDetails component with styling for order summary display

---
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -0,0 +1,37 @@
+import { formatDate } from '../../utils/formatDate';
+import './OrderDetails.css';
+
+export function OrderDetails({ order, address }) {
+    return (
+        <div className="order-details">
+            <h2>Resumen del pedido</h2>
+            <ul>
+                <li>Código<b>{order.id}</b></li>
+                <li>Fecha<b>{formatDate(order.date)}</b></li>
+            </ul>
+            <ul>
+                <li>Enviar
+                    <p>
+                        <b>{address.address} {address.address_number}</b>
+                        <span>{address.city}, {address.province}</span>
+                    </p>
+                </li>
+            </ul>
+            <ul>
+                <li>Cantidad de productos<b>{order.products.length}</b></li>
+                {order.discount ? <li>Descuento <b>{order.discount}%</b></li> : null}
+                {order.discount ?
+                    <li>Precio
+                        <p>
+                            <span className="discount">
+                                ${order.total_amount}
+                            </span>
+                            <b>${parseFloat(order.total_amount - (order.total_amount * order.discount) / 100)}</b>
+                        </p>
+                    </li> :
+                    <li>Precio<b>${order.total_amount}</b></li>
+                }
+            </ul>
+        </div>
+    )
+}
