26946138bbdcee7e56b1615a62956487563b1e5e
Agustín Concollato <agustinconcollato@gmail.com>
2025-01-17 13:17:16 -0300

    feat: enhance Footer component with modal functionality for terms and conditions

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,8 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import './Footer.css'
 import { Link } from 'react-router-dom'
 import { CategoriesContext } from '../../context/CategoriesContext'
+import { Modal } from '../Modal/Modal'
 import { AuthContext } from '../../context/AuthContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
@@ -11,10 +12,21 @@ export function Footer() {
     const { categories } = useContext(CategoriesContext)
     const { user, logOut } = useContext(AuthContext)
 
+    const [modal, setModal] = useState(null)
+
     return (
         <footer>
             <div>
                 <div className='container-footer-list'>
+                    <div className='footer-list'>
+                        <h4>Términos y condiciones</h4>
+                        <ul>
+                            <li><button onClick={() => setModal(1)}>Compras y envios</button></li>
+                            <li><button onClick={() => setModal(2)}>Medios de pago</button></li>
+                            <li><button onClick={() => setModal(3)}>Devoluciones</button></li>
+                            <li><button onClick={() => setModal(4)}>Cancelar pedido</button></li>
+                        </ul>
+                    </div>
                     <div className='footer-list'>
                         <h4>Categorías</h4>
                         <ul>
@@ -41,13 +53,28 @@ export function Footer() {
                         }
                     </div>
                 </div>
-                <div>
-                    <button className='btn'>Términos y condiciones</button>
-                    <button className='btn'>Medios de pago</button>
-                    <button className='btn'>Cancelar pedido</button>
-                </div>
                 <Link to={'/'} className='btn-whatsapp'><FontAwesomeIcon icon={faWhatsapp} size='2x' /></Link>
             </div>
+            {modal &&
+                <Modal onClose={setModal}>
+                    {modal == 1 ?
+                        <div>
+                            <h3>Compras y envios</h3>
+                            <p> Venta únicamente mayorista</p>
+                            <p> Envío a todo el país por transporte de preferencia. El costo del envío queda a cargo del cliente.</p>
+                        </div> :
+                        modal == 2 ?
+                            <div>
+                                <h3>Medios de pago</h3>
+                                <p>Depósitos y transferencias</p>
+                                <p>Efectivo</p>
+                            </div> :
+                            modal == 3 ?
+                                <div>3</div> :
+                                <div>4</div>
+                    }
+                </Modal>
+            }
         </footer>
     )
 }
