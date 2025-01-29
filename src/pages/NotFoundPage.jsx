3788950b199d1f448a40f11a3be3fe2bfbc9fba9
Agustín Concollato <agustinconcollato@gmail.com>
2025-01-29 02:24:11 -0300

    style: enhance NotFoundPage layout with minimum height for better visibility

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,11 +2,18 @@ import { Link } from 'react-router-dom'
 
 export function NotFoundPage() {
     return (
-        <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '20px' }}>
-            <h1 style={{fontSize: '36px'}}>404</h1>
+        <section style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '20px',
+            minHeight: 'calc(100vh - 375px)'
+        }}>
+            <h1 style={{ fontSize: '36px' }}>404</h1>
             <div>
-                <h2 style={{textAlign: 'center', fontWeight: '500'}}>Página no encontrada</h2>
-                <p style={{textAlign: 'center'}}>La página que buscas no existe o ha sido eliminada.</p>
+                <h2 style={{ textAlign: 'center', fontWeight: '500' }}>Página no encontrada</h2>
+                <p style={{ textAlign: 'center' }}>La página que buscas no existe o ha sido eliminada.</p>
             </div>
             <div>
                 <Link to="/" className="btn btn-solid">Volver al inicio</Link>
