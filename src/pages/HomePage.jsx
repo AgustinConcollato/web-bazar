f95115f71b311dad80faa8757d7a92f84c391426
Agustín Concollato <agustinconcollato@gmail.com>
2025-01-29 02:24:28 -0300

    feat: integrate address fetching for user in home page and display prompt for adding address

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Loading } from "../components/Loading/Loading";
 import { SectionCategoriesHome } from "../components/SectionCategoriesHome/SectionCategoriesHome";
 import { ProductCard } from "../components/ProductCard/ProductCard";
 import { api } from "api-services";
+import { AuthContext } from "../context/authContext";
+import { Address } from "api-services/addressService";
+import { Link } from "react-router-dom";
 
 export function HomePage() {
 
-    const { Products, Address } = api
+    const { Products } = api
+    const { user } = useContext(AuthContext)
 
     const [productsListDate, setProductsListDate] = useState(null);
     const [productsListViews, setProductsListViews] = useState(null);
+    const [addresses, setAddresses] = useState([]);
 
     async function getProductsByCreationDate() {
 
@@ -39,28 +44,45 @@ export function HomePage() {
         setProductsListViews(dataPage.data)
     }
 
+    async function getAddress() {
+        const address = new Address(user.uid)
+
+        const response = await address.get()
+        setAddresses(response)
+    }
+
     useEffect(() => {
         document.title = 'Bazarshop'
         getProductsByCreationDate()
         getProductsByViews()
-        // getAddress()
     }, [])
+
+    useEffect(() => {
+        user && getAddress()
+    }, [user])
 
 
     return (
         <>
-            <section className="section-products-home" style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
-                {productsListDate ?
-                    productsListDate.length > 0 ?
-                        productsListDate.map(e => <ProductCard key={e.id} e={e} />) :
+            {addresses.length == 0 &&
+                <section className="section-address-home">
+                    <p>Agrega una dirección para poder realizar tus pedidos <Link to={'/perfil'} className="btn btn-regular">Agrergar</Link></p>
+                </section>
+            }
+            <section className="section-products-home">
+                <h2 style={{ flex: 'none' }}>Más relevantes</h2>
+                {productsListViews ?
+                    productsListViews.length > 0 ?
+                        productsListViews.map(e => <ProductCard key={e.id} e={e} />) :
                         <p>No hay productos en esta categoría</p>
                     : <Loading />}
             </section>
             <SectionCategoriesHome />
-            <section className="section-products-home" style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
-                {productsListViews ?
-                    productsListViews.length > 0 ?
-                        productsListViews.map(e => <ProductCard key={e.id} e={e} />) :
+            <section className="section-products-home">
+                <h2 style={{ flex: 'none' }}>Nuevos ingresos</h2>
+                {productsListDate ?
+                    productsListDate.length > 0 ?
+                        productsListDate.map(e => <ProductCard key={e.id} e={e} />) :
                         <p>No hay productos en esta categoría</p>
                     : <Loading />}
             </section>
