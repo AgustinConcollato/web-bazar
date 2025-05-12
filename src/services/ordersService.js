718aa5f31322a7a7a455129378eb1d68c94d6bb5
Agustín Concollato <agustinconcollato@gmail.com>
2025-05-11 23:30:36 -0300

    refactor: update token retrieval and restructure order service methods; comment out unused methods for clarity and future implementation

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -2,149 +2,174 @@ import { urlOrder as url } from "./api";
 
 export class Order {
     constructor() {
-        this.token = localStorage.getItem('authToken')
+        this.token = localStorage.getItem('token')
     }
 
-    async create({ data }) {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${this.token}`
-            },
-            body: data
-        })
+    // async create({ data }) {
+    //     const response = await fetch(url, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Authorization': `Bearer ${this.token}`
+    //         },
+    //         body: data
+    //     })
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(JSON.stringify(errorData));
-        }
+    //     if (!response.ok) {
+    //         const errorData = await response.json();
+    //         throw new Error(JSON.stringify(errorData));
+    //     }
 
-        return await response.json()
-    }
+    //     return await response.json()
+    // }
 
-    async pending(id = null) {
+    // async pending(id = null) {
 
-        if (id) {
-            const response = await fetch(`${url}/pending/${id}`)
+    //     if (id) {
+    //         const response = await fetch(`${url}/pending/${id}`)
 
-            return await response.json()
-        }
+    //         return await response.json()
+    //     }
 
-        const response = await fetch(`${url}/pending`, {
-            headers: {
-                'Authorization': `Bearer ${this.token}`
-            }
-        })
-        
-        return await response.json()
-    }
+    //     const response = await fetch(`${url}/pending`, {
+    //         headers: {
+    //             'Authorization': `Bearer ${this.token}`
+    //         }
+    //     })
 
-    async accepted(id = null) {
-        try {
+    //     return await response.json()
+    // }
 
-            if (id) {
-                const response = await fetch(`${url}/accepted/${id}`)
+    // async accepted(id = null) {
+    //     try {
 
-                    if (!response.ok) {
-                        const error = await response.json()
-                        throw error
-                    }
+    //         if (id) {
+    //             const response = await fetch(`${url}/accepted/${id}`)
 
-                return await response.json()
-            }
+    //             if (!response.ok) {
+    //                 const error = await response.json()
+    //                 throw error
+    //             }
 
-            const response = await fetch(`${url}/accepted`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${this.token}`
-                }
-            })
-            
-            if (!response.ok) {
-                const error = await response.json()
-                throw error
-            }
+    //             return await response.json()
+    //         }
 
-            return await response.json()
+    //         const response = await fetch(`${url}/accepted`, {
+    //             method: 'GET',
+    //             headers: {
+    //                 'Authorization': `Bearer ${this.token}`
+    //             }
+    //         })
 
-        } catch (error) {
+    //         if (!response.ok) {
+    //             const error = await response.json()
+    //             throw error
+    //         }
 
-        }
-    }
+    //         return await response.json()
 
-    async rejectOrder(id) {
-        try {
-            const response = await fetch(`${url}/reject/${id}`, {
-                method: 'PUT',
-                headers: {
-                    'Authorization': `Bearer ${this.token}`
-                }
-            })
+    //     } catch (error) {
 
-            if (!response.ok) {
-                const error = await response.json()
-                throw error
-            }
+    //     }
+    // }
 
-            return await response.json()
-        } catch (error) {
-            throw error
-        }
-    }
+    // async rejectOrder(id) {
+    //     try {
+    //         const response = await fetch(`${url}/reject/${id}`, {
+    //             method: 'PUT',
+    //             headers: {
+    //                 'Authorization': `Bearer ${this.token}`
+    //             }
+    //         })
 
-    async acceptOrder(id) {
-        try {
-            const response = await fetch(`${url}/accept/${id}`, {
-                method: 'PUT',
-                headers: {
-                    'Authorization': `Bearer ${this.token}`
-                }
-            })
+    //         if (!response.ok) {
+    //             const error = await response.json()
+    //             throw error
+    //         }
 
-            if (!response.ok) {
-                const error = await response.json()
-                throw error
-            }
+    //         return await response.json()
+    //     } catch (error) {
+    //         throw error
+    //     }
+    // }
 
-            return await response.json()
-        } catch (error) {
-            throw error
-        }
-    }
+    // async acceptOrder(id) {
+    //     try {
+    //         const response = await fetch(`${url}/accept/${id}`, {
+    //             method: 'PUT',
+    //             headers: {
+    //                 'Authorization': `Bearer ${this.token}`
+    //             }
+    //         })
 
-    async completed({ clientId, year, month }) {
-        let urlRequest = `${url}/completed`;
+    //         if (!response.ok) {
+    //             const error = await response.json()
+    //             throw error
+    //         }
 
-        if (clientId) {
-            urlRequest += `/${clientId}`;
-        }
+    //         return await response.json()
+    //     } catch (error) {
+    //         throw error
+    //     }
+    // }
+
+    // async completed({ clientId, year, month }) {
+    //     let urlRequest = `${url}/completed`;
+
+    //     if (clientId) {
+    //         urlRequest += `/${clientId}`;
+    //     }
+
+    //     // Construimos los parámetros de la URL
+    //     const params = new URLSearchParams();
+    //     params.append("year", year);
+
+    //     if (month !== undefined && month !== "all") {
+    //         params.append("month", month);
+    //     }
+
+    //     // Concatenamos los parámetros solo si existen
+    //     const fullUrl = `${urlRequest}?${params.toString()}`;
+    //     const response = await fetch(fullUrl);
+
+    //     return await response.json();
+    // }
+
+    async get({ status, clientId, year, month, page }) {
 
         // Construimos los parámetros de la URL
         const params = new URLSearchParams();
-        params.append("year", year);
+        
+        params.append("page", page);
+
+        if (status) {
+            params.append("status", status);
+        }
+
+        if (year) {
+            params.append("year", year);
+        }
+
+        if (clientId) {
+            params.append("client_id", clientId);
+        }
 
         if (month !== undefined && month !== "all") {
             params.append("month", month);
         }
 
         // Concatenamos los parámetros solo si existen
-        const fullUrl = `${urlRequest}?${params.toString()}`;
-        const response = await fetch(fullUrl);
+        const fullUrl = `${url}/client?${params.toString()}`;
+        const response = await fetch(fullUrl, {
+            headers: {
+                'Authorization': `Bearer ${this.token}`
+            }
+        })
 
-        return await response.json();
+        return await response.json()
     }
 
-    async getAll(userId) {
-        try {
-            const response = await fetch(`${url}/user/${userId}`)
-            const orders = await response.json()
-            return orders
-        } catch (error) {
-            console.log('error al obtener todos los pedidos', error)
-        }
-    }
 
-    async get(id) {
+    async detail(id) {
         try {
             const response = await fetch(`${url}/detail/${id}`)
 
