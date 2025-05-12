ff1bfa0fbb37368c234b297acac7bbabdb6463e1
Agustín Concollato <agustinconcollato@gmail.com>
2025-05-11 23:31:08 -0300

    feat: implement email change functionality in VerifyEmailPage; add form for updating email with validation and error handling

--- src/pages/VerifyEmailPage/VerifyEmailPage.jsx
+++ src/pages/VerifyEmailPage/VerifyEmailPage.jsx
@@ -10,19 +10,23 @@ export function VerifyEmailPage() {
 
     const { client } = useContext(AuthContext)
 
-    const { Mail } = api
+    const { Mail, Auth } = api
     const mail = new Mail()
+    const auth = new Auth()
 
     const [emailSent, setEmailSent] = useState(false)
     const [code, setCode] = useState(false)
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const [loadingResend, setLoadingResend] = useState(false)
+    const [updateEmail, setUpdateEmail] = useState(false)
+    const [errorEmail, setErrorEmail] = useState('')
+    const [loadingEmail, setLoadingEmail] = useState(false)
 
-    async function sendEmailVerification() {
+    async function sendEmailVerification(email) {
         setLoading(true)
         try {
-            const response = await mail.verify(client.email)
+            const response = await mail.verify(email)
 
             if (response) {
                 setEmailSent(true)
@@ -40,7 +44,7 @@ export function VerifyEmailPage() {
     async function resendCode() {
         setLoadingResend(true)
         try {
-            const response = await mail.verify(client.email)
+            const response = await mail.verify(email)
 
             if (response) {
                 setEmailSent(true)
@@ -81,6 +85,40 @@ export function VerifyEmailPage() {
         }
     }
 
+    async function handleEmailChange(e) {
+        e.preventDefault()
+
+        const email = { email: e.target.email.value }
+
+        // comprobar si el nuevo correo electrónico es igual al actual
+        if (email.email == client.email) {
+            setErrorEmail('El correo electrónico es el mismo que el actual')
+            return
+        }
+
+        setErrorEmail('')
+
+        try {
+            setLoadingEmail(true)
+            const response = await auth.updateEmail(email)
+            if (response) {
+                client.email = response.email
+                setUpdateEmail(false)
+                setEmailSent(false)
+            }
+        } catch (error) {
+            if (error.errors.email[0] == 'The email has already been taken.') {
+                setErrorEmail('El correo electrónico ya está en uso')
+            }
+
+            if (error.errors.email[0] == "The email field is required.") {
+                setErrorEmail('Completa con el correo electrónico')
+            }
+        } finally {
+            setLoadingEmail(false)
+        }
+    }
+
     useEffect(() => {
         document.title = 'Verifica tu correo electrónico'
 
@@ -90,7 +128,7 @@ export function VerifyEmailPage() {
             }
 
             if (!client.email_verified_at && !emailSent) {
-                sendEmailVerification()
+                sendEmailVerification(client.email)
             }
         }
 
@@ -103,7 +141,7 @@ export function VerifyEmailPage() {
                     <div>
                         <h2>Verifica tu correo electrónico</h2>
                         <p>
-                            Hemos enviado un código de verificación a <b>{emailSent && client.email}</b>.
+                            Hemos enviado un código de verificación a <b>{emailSent && client.email}</b>
                         </p>
                     </div>
 
@@ -121,9 +159,21 @@ export function VerifyEmailPage() {
                     {error && <p className="message-error">{error} <FontAwesomeIcon icon={faXmark} onClick={() => setError('')} /> </p>}
                     <div>
                         <p>Si no encuentras el correo, revisa tu carpeta de spam o intenta nuevamente</p>
-                        <button onClick={resendCode} className="btn btn-regular" disabled={loadingResend}>{loadingResend ? <FontAwesomeIcon icon={faCircleNotch} spin /> : 'Reenviar código'}</button>
+                        <button onClick={() => resendCode(client.email)} className="btn btn-regular" disabled={loadingResend}>{loadingResend ? <FontAwesomeIcon icon={faCircleNotch} spin /> : 'Reenviar código'}</button>
                     </div>
-
+                    <div className="update-email">
+                        <p>Si te equivocaste o simplemente quieres cambiar tu correo electrónico, puedes hacerlo aquí <button onClick={() => setUpdateEmail(true)} className="btn-change-email"><b>Cambiar correo electrónico</b></button></p>
+                        {updateEmail &&
+                            <form onSubmit={handleEmailChange}>
+                                <input type="email" placeholder="Nuevo correo electrónico" className="input" name="email" />
+                                <div>
+                                    <button type="button" className="btn" onClick={() => setUpdateEmail(false)}>Cancelar</button>
+                                    <button type="submit" className="btn btn-solid" disabled={loadingEmail}>{loadingEmail ? <FontAwesomeIcon icon={faCircleNotch} spin /> : 'Cambiar correo electrónico'}</button>
+                                </div>
+                                {errorEmail && <p className="message-error">{errorEmail} <FontAwesomeIcon icon={faXmark} onClick={() => setErrorEmail('')} /> </p>}
+                            </form>
+                        }
+                    </div>
                 </div> :
                 <div className="loading-container">
                     <p>Enviando correo de verificación...</p>
