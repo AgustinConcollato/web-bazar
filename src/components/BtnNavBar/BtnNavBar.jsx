de42d5fe6bb907e446135d80b739bdec0f62e3b6
Agustín Concollato <agustinconcollato@gmail.com>
2025-08-01 06:28:50 -0300

    feat: exclude specific category from navigation bar in BtnNavBar component for improved category filtering

--- src/components/BtnNavBar/BtnNavBar.jsx
+++ src/components/BtnNavBar/BtnNavBar.jsx
@@ -21,6 +21,7 @@ export function BtnNavBar() {
                     <ul>
                         {(categories && categories.length != 0) &&
                             categories.map(e =>
+                                e.code != 'CAT007' &&
                                 <li key={e.code} onClick={() => setHidden(!hidden)}>
                                     <Link to={'/productos/' + e.code}>{e.name}</Link>
                                 </li>
